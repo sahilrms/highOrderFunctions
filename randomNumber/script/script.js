60f26cba6f2c1db4076efdfd393a25050f8013ad
sahilrms <sahilwethehelpers@gmail.com>
2023-04-12 12:48:58 +0530

    Update script.js

--- randomNumber/script/script.js
+++ randomNumber/script/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+randomNumber();
 let delay = 0;
 let initalInterval = setInterval(() => { randomNumber(); }, 3000)
 let newInterval;
@@ -8,21 +8,8 @@ function newDelay() {
     clearInterval(newInterval)
         delay = Number(document.querySelector('#string_text').value);
         newInterval = setInterval(() => { randomNumber(); }, delay * 1000)
-
-
 }
 function randomNumber() {
     let rev = Math.floor(Math.random() * 1000);
     document.getElementById('reversed').innerHTML = `<h1> ${rev}</h1>`;
-=======
-let btn=document.querySelector('#reverse');
-btn.addEventListener('click',validateURL);
-function validateURL() 
-{
-    let text=document.querySelector('#string_text');
-    let rev= text.value
-    let chkString = /^https:\/\/www.linkedin.com\/in\/[a-z||A-Z||0-9||_||-]{5,30}$/gm;
-    let result=chkString.test(rev);
-    document.getElementById('reversed').innerHTML=`<h1>${result?'Profile link is valid with proper length':'Profile link is not valid'}</h1>`;
->>>>>>> 16a1e42a1ab8aba1a99374cdc46f6bd24115c703
 }
